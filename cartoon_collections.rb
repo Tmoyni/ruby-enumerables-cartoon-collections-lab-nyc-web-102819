a584d6aae763cf23a2dffd335ea7ed49873eaef1
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-05 20:50:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,13 @@ def long_planeteer_calls(array)
   array.any? { |call| call.length > 4 }
 end
 
+
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.detect { |element| element == cheese_types }
+
+  array.find do |element|
+    cheese_types.include?(element)
   end
+end
+
+find_the_cheese(array)  
