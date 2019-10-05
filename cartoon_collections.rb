ea055a80c16d8b0923d06ab10c35cd1a011c6675
Triona Moynihan <trionamoynihan@gmail.com>
2019-10-05 20:30:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include { |snacks| snacks == cheese_types }
+  array.include { |element| element == cheese_types }
   end
